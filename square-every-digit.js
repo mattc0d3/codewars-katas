fafcffbd9fb4dc057c96b7afd6a4a2eda8e998b2
Matt <matthew.sheehan94@gmail.com>
2023-08-23 10:17:32 +0100

    implement solution for square every digit kata

---
+++ square-every-digit.js
@@ -0,0 +1,8 @@
+function squareDigits(num) {
+    const digitsArray = num.toString().split("")
+    const squaredDigitsArray = digitsArray.map(digit => Number(digit) ** 2)
+    return squaredDigitsArray.join("");
+}
+
+console.log(squareDigits(9119))
+console.log(squareDigits(765))
