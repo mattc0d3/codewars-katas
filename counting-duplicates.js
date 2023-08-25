bb08082d10cd9816b5b857d0b7dbf1bb2d9ccdc7
Matt <matthew.sheehan94@gmail.com>
2023-08-25 09:19:31 +0100

    first run through attempt at counting duplicates

---
+++ counting-duplicates.js
@@ -0,0 +1,18 @@
+function duplicateCount(text) {
+    const seen = {}
+    let count = 0
+    for (let i = 0; i < text.length; i++) {
+        let char = text[i].toLowerCase()
+        if (!seen[char]) seen[char] = true
+        else count++
+    }
+    return count
+}
+
+console.log(duplicateCount("abcde")) // 0
+console.log(duplicateCount("aabbcde")) // 2
+console.log(duplicateCount("aabBcde")) // 2
+console.log(duplicateCount("indivisibility")) // 1
+console.log(duplicateCount("Indivisibilities")) // 2
+console.log(duplicateCount("aA11")) // 2
+console.log(duplicateCount("ABBA")) // 2
