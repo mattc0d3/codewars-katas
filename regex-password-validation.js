8d1e1b9d58c03acd90b05d16b461dc50f9fb7a46
Matt <matthew.sheehan94@gmail.com>
2023-09-12 15:50:39 +0100

    implement solution for regex password validation kata

--- regex-password-validation.js
+++ regex-password-validation.js
@@ -1,1 +1,13 @@
-const REGEXP = /[a-z][A-Z]{5}/;
+const REGEXP = /^(?=[^a-z]*[a-z]+)(?=[^A-Z]*[A-Z]+)(?=\D*\d)\w{6,}$/g;
+
+console.log(REGEXP.test("fjd3IR9")) // true
+console.log(REGEXP.test("ghdfj32")) // false
+console.log(REGEXP.test("DSJKHD23")) // false
+console.log(REGEXP.test("dsF43")) // false
+console.log(REGEXP.test("4fdg5Fj3")) // true
+console.log(REGEXP.test("DHSJdhjsU")) // false
+console.log(REGEXP.test("fjd3IR9.;")) // false
+console.log(REGEXP.test("fjd3  IR9")) // false
+console.log(REGEXP.test("djI38D55")) // true
+console.log(REGEXP.test("djI3_8D55")) // false
+console.log(REGEXP.test("djI38D55@@")) // false
