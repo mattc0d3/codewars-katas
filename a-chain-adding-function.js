f752a5d98c9fe1d0f18378fc5f7b72e80045c1d9
Matt <matthew.sheehan94@gmail.com>
2023-09-14 16:48:33 +0100

    setup incomplete chain adding function (curry)

---
+++ a-chain-adding-function.js
@@ -0,0 +1,14 @@
+function add(a) {
+    return (b) => {
+        return (c) => {
+            return a + b + c
+
+        }
+    }
+}
+
+console.log(add(1)) // 1
+console.log(add(1)(2)) // 3
+console.log(add(1)(2)(3)) // 6
+// console.log(add(1)(2)(3)(4)) // 10
+// console.log(add(1)(2)(3)(4)(5)) // 15
